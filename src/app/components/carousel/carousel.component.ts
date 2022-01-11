3c40aa53b21da9ea4237ac9f2bcaf0763b147eb6
Chris Harden <hawkeyeul@gmail.com>
2022-01-10 21:06:29 -0600

    updates to carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,3 +1,5 @@
+import { NONE_TYPE } from '@angular/compiler';
+import { FnParam } from '@angular/compiler/src/output/output_ast';
 import {
   AfterContentInit,
   Component,
@@ -9,7 +11,7 @@ import {
   QueryList,
   Renderer2,
 } from '@angular/core';
-import { delay, filter } from 'rxjs';
+import { delay, filter, first } from 'rxjs';
 import { CarouselItemComponent } from './carousel-item/carousel-item.component';
 import { ICarouselItem } from './interfaces/ICarouselItem';
 
@@ -19,59 +21,131 @@ import { ICarouselItem } from './interfaces/ICarouselItem';
   styleUrls: ['./carousel.component.less'],
 })
 export class CarouselComponent implements OnInit, AfterContentInit {
-  currentIndex: number = 0;
-  public carouselItemArray: boolean[]=[];
+  public activeIndex: number = 0;
+  public previousIndex: number = 0;
+  public itemArray: any[] = [];
+  private slideFrom: any = null;
+  private previousElement?: ElementRef;
+  @ContentChildren('carouseItem') carouselItems?: QueryList<ElementRef>;
+  @Input() wrapping: boolean = true;
 
   constructor(private elemntRef: ElementRef, private render: Renderer2) {}
-  @ContentChildren('carouseItem') carouselItems?: QueryList<ElementRef>;
 
   ngAfterContentInit(): void {
-    let afterActive = false;
-     this.carouselItems?.map((el: ElementRef, idx) => {
-      const nativeEl = el.nativeElement;
-       const active = nativeEl.classList.contains('active');
-       this.setElementClass(idx);
-      return active;
-    });
+    console.log(this.carouselItems?.length);
+    this.setInitialActiveSlide();
   }
   ngOnInit(): void {}
 
-  selectSlide(index: number): void {
-    const selEl = this.carouselItems?.get(index);
-    const curEl = this.carouselItems?.get(this.currentIndex);
-    if (selEl){
-      this.render.addClass(selEl.nativeElement, 'active');
+  setInitialActiveSlide(): void {
+    let foundActive = false;
+    this.carouselItems?.forEach((el: ElementRef, idx) => {
+      const nativeEl = el.nativeElement;
+      if (nativeEl.classList.contains('active')) {
+        foundActive = true;
+        this.activeIndex = idx;
+      } else this.render.addClass(nativeEl, 'hidden');
+    });
+    const first = this.carouselItems?.get(0)?.nativeElement;
+    if(!foundActive) {
+      this.removeUnusedClassesFromElement(first);
+      this.render.addClass(first, 'active');
+      this.activeIndex = 0;
     }
-    this.currentIndex = index;
-    this.setElementClass(this.currentIndex);
   }
 
-  setElementClass(index:number):void{
-    let isActive = false;
-    this.carouselItemArray = [];
-    this.carouselItems?.map((el,idx) => {
-      const nativeEl = el.nativeElement;
-      let activeIndex = Array.from(nativeEl.classList).indexOf('active');
-      if (idx === this.currentIndex){
-        this.render.removeClass(nativeEl, 'left');
-        this.render.removeClass(nativeEl, 'right');
-        this.render.removeClass(nativeEl,'active');
-        this.render.addClass(nativeEl, 'active');
-      }
-      if (activeIndex > -1) {
-        isActive = true;
-        this.carouselItemArray.push(true);
+  slideTo(newActiveEl:ElementRef, curentEl: ElementRef, previousEl: ElementRef, direction:string) {
+    //set Class For Active
+    this.removeUnusedClassesFromElement(newActiveEl);
+    this.render.addClass(newActiveEl, 'active');
+    // setClassForPrevious
+    this.removeUnusedClassesFromElement(previousEl);
+    this.removeUnusedClassesFromElement(curentEl);
+    this.render.addClass(curentEl, direction);
+  }
 
+  slideDirection(previousIndx:number):string{
+    console.log(previousIndx, this.activeIndex);
+    if (previousIndx < this.activeIndex){
+      return 'right';
+    } else if (previousIndx > this.activeIndex){
+      return 'left';
+    }
+    return '';
+  }
+
+  ableToSlide(): boolean {
+    if (this.carouselItems?.length && this.carouselItems.length > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  previousSlide(): void {
+    this.ableToSlide() && this.selectSlide(this.activeIndex - 1);
+  }
+
+  nextSlide(): void {
+    this.ableToSlide() && this.selectSlide(this.activeIndex + 1);
+  }
+
+  removeUnusedClassesFromElement(element: ElementRef) {
+    this.render.removeClass(element, 'hidden');
+    this.render.removeClass(element, 'active');
+    this.render.removeClass(element, 'left');
+    this.render.removeClass(element, 'right');
+  }
+
+  getNewActiveIndex(index: number): number {
+    if (this.carouselItems?.length) {
+      const itemsMaxIndex = this.carouselItems?.length - 1;
+      if (!this.wrapping) {
+        if (index > itemsMaxIndex) {
+          return itemsMaxIndex;
+        } else if (index < 0) {
+          return 0;
+        }
       } else {
-        this.carouselItemArray.push(false);
-      if(!isActive){
-        this.render.addClass(nativeEl,'left');
-      } else {
-        this.render.addClass(nativeEl, 'right');
+        if(index > itemsMaxIndex){
+          return 0;
+        }
+        if(index < 0){
+          return itemsMaxIndex;
+        }
       }
+    }
+    return index;
+  }
+
+  selectSlide(selectedIndex: number): void {
+    if (selectedIndex == this.activeIndex) return;
+
+    const currentIndex = this.activeIndex;
+    this.activeIndex = this.getNewActiveIndex(selectedIndex);
+
+    const activeEl = this.carouselItems?.get(this.activeIndex)?.nativeElement;
+    const previousEl = this.carouselItems?.get(this.previousIndex)?.nativeElement;
+    const currentEl = this.carouselItems?.get(currentIndex)?.nativeElement;
+    this.removeUnusedClassesFromElement(activeEl);
+    this.removeUnusedClassesFromElement(currentEl);
+    const slideDir = this.slideDirection(currentIndex);
+    this.setElementDirection(slideDir, activeEl, currentEl, previousEl);
+  }
+
+  setElementDirection(
+    slideDir: string,
+    newActiveEl: ElementRef,
+    currentEl: ElementRef,
+    previousEl: ElementRef
+  ): void {
+    this.itemArray = new Array(this.carouselItems?.length);
+    this.carouselItems?.forEach((el, idx) => {
+      const currentEl = el.nativeElement;
+      if (currentEl === newActiveEl) {
+        if (slideDir) {
+          this.slideTo(newActiveEl, currentEl, previousEl, slideDir);
+        }
       }
-
-
     });
   }
 }
