e6e9712936c2ec84e6055468bc6b27bd82df7817
Chris Harden <hawkeyeul@gmail.com>
2022-01-12 10:36:01 -0600

    typeahead finished

--- src/app/components/typeahead/typeahead.component.ts
+++ src/app/components/typeahead/typeahead.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
 import { TypeAheadItem } from 'src/app/interfaces/typeAheadItem';
 
 @Component({
@@ -11,23 +17,38 @@ export class TypeaheadComponent implements OnInit {
   public typeaheadData = ['bo', 'dave', 'fran', 'clive', 'danny', 'Ralf'];
   public filteredData: TypeAheadItem[] = [];
   public showList = false;
-  private mouseInLIst =false;
-  public currentLIstIndex = 0;
-  public inputFocused:boolean = false;
+  private mouseInLIst = false;
+  public currentLIstIndex = -1;
+  public inputFocused: boolean = false;
   constructor() {}
   @Input() CaseSensitive: boolean = false;
   ngOnInit(): void {}
+
   onChange(value: string): void {
     this.typeAheadValue = value;
-    this.checkDataForValue(value);
+    if (value) {
+    this.checkDataForValue(value)
+    } else {
+      this.filteredData = [];
+      this.currentLIstIndex = -1;
+    }
   }
 
-  selectFromDropDown(item:TypeAheadItem){
+  selectFromDropDown(item: TypeAheadItem) {
     this.typeAheadValue = item.value;
     this.filteredData = [];
     this.showList = false;
   }
 
+  inputInFocus() {
+    this.currentLIstIndex = 0;
+    this.inputFocused = true;
+    console.log(this.inputFocused);
+  }
+
+  inputBlurred() {
+    this.inputFocused = false;
+  }
 
   checkDataForValue(value: string): void {
     this.filteredData = [];
@@ -38,44 +59,48 @@ export class TypeaheadComponent implements OnInit {
       } else {
         found = str.toLowerCase().includes(value.toLowerCase());
       }
-      if(found) {
-
+      if (found) {
         this.filteredData.push({
           value: str,
-          text: str.replace(value,`<strong class="testme">${value}</strong>`)
+          text: str.replace(value, `<strong class="testme">${value}</strong>`),
         });
         return;
       }
     });
-    this.showList = (this.filteredData.length > 0)
+    this.showList = this.filteredData.length > 0;
     this.currentLIstIndex = 0;
   }
 
   downKey() {
-    (this.filteredData.length > 0 && this.currentLIstIndex < this.filteredData.length -1) && this.currentLIstIndex ++;
-    console.log(this.currentLIstIndex);
-
+    this.filteredData.length > 0 &&
+      this.currentLIstIndex < this.filteredData.length - 1 &&
+      this.currentLIstIndex++;
   }
 
   upKey() {
-    (this.filteredData.length > 0 && this.currentLIstIndex > 0) && this.currentLIstIndex --;
-    console.log(this.currentLIstIndex);
+    this.filteredData.length > 0 &&
+      this.currentLIstIndex > 0 &&
+      this.currentLIstIndex--;
   }
 
   enterKey() {
-    (this.filteredData.length > 0) && this.selectFromDropDown(this.filteredData[this.currentLIstIndex]);
-
+    this.filteredData.length > 0 &&
+      this.selectFromDropDown(this.filteredData[this.currentLIstIndex]);
   }
 
-  keyPress(event:any){
-  if (event.keyCode === 38) {
-    this.upKey();
+  keyPress(event: any) {
+    if (event.keyCode === 38) {
+      this.upKey();
+    }
+    if (event.keyCode === 40) {
+      this.downKey();
+    }
+    if (event.keyCode === 13) {
+      this.enterKey();
+    }
   }
-  if (event.keyCode === 40){
-    this.downKey();
-  }
-  if(event.keyCode === 13){
-    this.enterKey();
-  }
+
+  selectItem(idx: number): boolean {
+    return idx === this.currentLIstIndex;
   }
 }
